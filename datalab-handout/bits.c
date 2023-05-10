b8bd198472db30ead0583ac3556463c96b449014
Miao <one.lemorage@gmail.com>
2023-05-10 22:50:09 +0000

    done with data lab

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+ * name: Glenn Miao | userid: lemorage
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
@@ -140,7 +140,11 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+  /* exploit ability of De Morgan's laws to achieve the functionality of bitwise XOR */
+  int a = ~(x & y);
+  int b = ~(~x & ~y);
+  int result = a & b;
+  return result;
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -149,7 +153,8 @@ int bitXor(int x, int y) {
  *   Rating: 1
  */
 int tmin(void) {
-  return 2;
+  /* exploit ability of shifts to compute tmin */
+  return 1 << 31;
 }
 //2
 /*
@@ -160,7 +165,18 @@ int tmin(void) {
  *   Rating: 2
  */
 int isTmax(int x) {
-  return 2;
+  /* exploit relationship of Tmin and Tmax to detect Tmax */
+  /* Tmax is a number where all bits are 1 except the sign bit. */
+  
+  /* First, check if x + 1 is equal to -(2^31), which is Tmin.
+     If it is, then x is Tmax. 
+     Therefore, sum = x + 1 and neg = ~x should be equal.
+     But we are also checking that x is not Tmin since Tmin
+     is its own additive inverse.
+     We use the double negation to check that neg is not 0. */
+  int sum = x + 1;
+  int neg = ~x;
+  return !((sum ^ neg) | !neg); // !(sum ^ neg) & !!neg
 }
 /* 
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
@@ -170,7 +186,12 @@ int isTmax(int x) {
  *   Rating: 2
  */
 int allOddBits(int x) {
-  return 2;
+  /* create a mask with every other bit set to 1 */
+  int mask = (0xAA | (0xAA << 8));
+  /* shift the mask to cover all odd-numbered bits */
+  mask = mask | (mask << 16);
+  /* check if all odd-numbered bits in x are set to 1 */
+  return !((x & mask) ^ mask);
 }
 /* 
  * negate - return -x 
@@ -180,7 +201,8 @@ int allOddBits(int x) {
  *   Rating: 2
  */
 int negate(int x) {
-  return 2;
+  /* exploit ability of (~x + 1) to compute the -x */
+  return ~x + 1;
 }
 //3
 /* 
@@ -193,7 +215,21 @@ int negate(int x) {
  *   Rating: 3
  */
 int isAsciiDigit(int x) {
-  return 2;
+  // Initialize variables for sign bit and limits
+  int signBit = 1 << 31;
+
+  int upperLimit = ~(signBit | 0x39);
+  
+  int lowerLimit = ~0x30;
+
+  // Check if x is less than upper limit
+  int lessThanUpper = signBit & ((upperLimit + x) >> 31);
+
+  // Check if x is greater than lower limit
+  int greaterThanLower = signBit & ((lowerLimit + 1 + x) >> 31);
+
+  // Return 1 if x is within the range of [0x30, 0x39]
+  return !(lessThanUpper | greaterThanLower);
 }
 /* 
  * conditional - same as x ? y : z 
@@ -203,7 +239,12 @@ int isAsciiDigit(int x) {
  *   Rating: 3
  */
 int conditional(int x, int y, int z) {
-  return 2;
+  /* create a mask that will be either all ones (if x is non-zero)
+     or all zeros (if x is zero) */
+  int mask = ~(!!x) + 1;
+  /* use the mask and bitwise AND to select between y and z */
+  int result = (mask & y) | (~mask & z);
+  return result;
 }
 /* 
  * isLessOrEqual - if x <= y  then return 1, else return 0 
@@ -213,7 +254,20 @@ int conditional(int x, int y, int z) {
  *   Rating: 3
  */
 int isLessOrEqual(int x, int y) {
-  return 2;
+  int sign_x = (x >> 31) & 1;
+  int sign_y = (y >> 31) & 1;
+
+  // If the signs are different, then x is less than y if x is negative
+  int diff_sign = sign_x ^ sign_y; // 0
+  int neg_x = sign_x & 1;
+  int less_if_diff_sign = (diff_sign & neg_x);
+
+  // If the signs are the same, then x is less than y if y - x is non-negative
+  int same_sign = !diff_sign;
+  int diff = y + (~x + 1); // y - x
+  int non_neg_diff_xy = ~(diff >> 31);
+
+  return less_if_diff_sign | (same_sign & non_neg_diff_xy);
 }
 //4
 /* 
@@ -225,7 +279,12 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4 
  */
 int logicalNeg(int x) {
-  return 2;
+  // if x is 0, then ~x is also 0.
+  // if x is nonzero, then x | ~x must be -1 (Sign Extension)
+  int negX = ~x + 1;  // negate x
+  int sign = (x | negX) >> 31; // get the sign bit of x or -x
+
+  return sign + 1; // return 1 if x is 0, otherwise 0
 }
 /* howManyBits - return the minimum number of bits required to represent x in
  *             two's complement
@@ -240,7 +299,52 @@ int logicalNeg(int x) {
  *  Rating: 4
  */
 int howManyBits(int x) {
-  return 0;
+  /* exploit ability of binary search to count the bits */
+
+  int sign = x >> 31;  // extract sign bit  
+  int bitCount = 0, check;
+
+  // x = (x ^ sign) + (~sign + 1);
+  x = (~x & sign) | (~sign & x);  // get absolute value of x
+
+  bitCount += !!(x >> 16) << 4;  // check upper 16 bits
+  x >>= bitCount;
+
+  bitCount += (check = !!(x >> 8) << 3);  // check next 8 bits
+  x >>= check;
+  
+  bitCount += (check = !!(x >> 4) << 2);  // check next 4 bits
+  x >>= check;
+
+  bitCount += (check = !!(x >> 2) << 1);  // check next 2 bits
+  x >>= check;
+
+  bitCount += (check = !!(x >> 1));  // check last bit
+  x >>= check;
+
+  return bitCount + x + 1; // account for sign bit
+
+  /* A cheating way of ...
+
+  int sign = x >> 31;  // sign bit
+  x = (sign & ~x) | (~sign & x);  // if x is negative, get its two's complement
+  int numBits = 0;
+
+  numBits = (!!x) + (!!((x >> 1) & 0x7FFFFFFF)) + (!!((x >> 2) & 0x3FFFFFFF)) + (!!((x >> 3) & 0x1FFFFFFF)) +
+            (!!((x >> 4) & 0x0FFFFFFF)) + (!!((x >> 5) & 0x07FFFFFF)) + (!!((x >> 6) & 0x03FFFFFF)) + 
+            (!!((x >> 7) & 0x01FFFFFF)) + (!!((x >> 8) & 0x00FFFFFF)) + (!!((x >> 9) & 0x007FFFFF)) +
+            (!!((x >> 10) & 0x003FFFFF)) + (!!((x >> 11) & 0x001FFFFF)) + (!!((x >> 12) & 0x000FFFFF)) + 
+            (!!((x >> 13) & 0x0007FFFF)) + (!!((x >> 14) & 0x0003FFFF)) + (!!((x >> 15) & 0x0001FFFF)) + 
+            (!!((x >> 16) & 0x0000FFFF)) + (!!((x >> 17) & 0x00007FFF)) + (!!((x >> 18) & 0x00003FFF)) + 
+            (!!((x >> 19) & 0x00001FFF)) + (!!((x >> 20) & 0x00000FFF)) + (!!((x >> 21) & 0x000007FF)) + 
+            (!!((x >> 22) & 0x000003FF)) + (!!((x >> 23) & 0x000001FF)) + (!!((x >> 24) & 0x000000FF)) + 
+            (!!((x >> 25) & 0x0000007F)) + (!!((x >> 26) & 0x0000003F)) + (!!((x >> 27) & 0x0000001F)) + 
+            (!!((x >> 28) & 0x0000000F)) + (!!((x >> 29) & 0x00000007)) + (!!((x >> 30) & 0x00000003)) + 
+            (!!((x >> 31) & 0x00000001));
+  
+  return numBits + 1;
+  
+  */
 }
 //float
 /* 
@@ -255,7 +359,20 @@ int howManyBits(int x) {
  *   Rating: 4
  */
 unsigned float_twice(unsigned uf) {
-  return 2;
+  unsigned sign = uf & (1 << 31); // get the sign bit
+  unsigned exponent = uf & 0x7F800000; // get the exponent field
+  unsigned mantissa = uf & 0x007FFFFF; // get the fraction field
+
+  if (exponent == 0x7F800000)
+    return uf;  // NaN or infinity
+
+  if (!exponent)
+    mantissa <<= 1;  // denormalized number or zero
+  else
+    if ((exponent += 0x00800000) == 0x7F800000) // normalized number
+      return sign | exponent;  // overflow to infinity
+
+  return sign | exponent | mantissa;
 }
 /* 
  * float_i2f - Return bit-level equivalent of expression (float) x
@@ -267,7 +384,28 @@ unsigned float_twice(unsigned uf) {
  *   Rating: 4
  */
 unsigned float_i2f(int x) {
-  return 2;
+  int neg = 1 << 31; // create a mask (0x80000000)
+  int sign = x & neg; // extract the sign bit
+  int exponent = 31; // initialize the exponent
+  int bias = 127;
+  int mantissa;
+  
+  if (!x) // if x is zero, return zero
+    return x;
+  if (x == neg) // if x is the TMin, return the its float representation
+    return neg | ((exponent + bias) << 23);
+  
+  // get abs(x)
+  x = sign ? ~x + 1 : x;
+  while (!(x & neg)) // left shift x and count the number of shifts in exponent
+    x <<= 1, --exponent;
+
+  // extract the mantissa bits by shifting x right by 8 bits
+  mantissa = (((~neg) & x) >> 8);
+  if (x & 0x80 && ((mantissa & 1) || ((x & 0x7f)))) // perform rounding if necessary
+    ++mantissa;
+
+  return sign + ((exponent + bias) << 23) + mantissa;
 }
 /* 
  * float_f2i - Return bit-level equivalent of expression (int) f
@@ -282,5 +420,31 @@ unsigned float_i2f(int x) {
  *   Rating: 4
  */
 int float_f2i(unsigned uf) {
-  return 2;
+  // extract the sign, exponent, and fraction/mantissa from uf
+  unsigned sign = uf >> 31;
+  unsigned exponent = (uf >> 23) & 0xFF;
+  unsigned mantissa = uf & 0x7FFFFF;
+  int bias = 0x7F;
+  int result;
+
+  // handle special cases: NaN, infinity, and denormalized numbers
+  if (exponent == 0xFF)
+    return 0x80000000u;
+  if (exponent < bias)
+    return 0;
+
+  // add the implicit leading 1 for normalized numbers
+  // mantissa = exponent ? mantissa | 0x80000000 : mantissa | 0x800000;
+
+  // adjust the exponent bias
+  exponent -= bias;
+
+  // shift the fraction based on the exponent
+  if (exponent >= 31)
+    return 0x80000000u;  // out of range
+  
+  result = ((exponent > 22) ? mantissa << (exponent - 23) : mantissa >> (23 - exponent)) + (1 << exponent);
+
+  // Apply the sign
+  return sign ? -result : result; // (result ^ sign) - sign;
 }
